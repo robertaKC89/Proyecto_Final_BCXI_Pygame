8fb2d3d4c626d05e764dc8fc94976b60557cad2b
Marta <neurotecnoedu@gmail.com>
2022-08-26 09:32:52 +0200

    feat: limito recorrido de nave

--- pilotando/entidades.py
+++ pilotando/entidades.py
@@ -14,10 +14,25 @@ from . import ANCHO, ALTO
 """
 
 class Nave (Sprite):
+    velocidad = 5
+
     def __init__(self) -> None:
         super().__init__()  #llamo a la class padre Sprite
-        img_nave = os.path.join ("scripts", "images", "NAVE3_redim (2).png")
+        img_nave = os.path.join ("scripts", "images", "NAVE3_redim.png")
         self.image = pg.image.load (img_nave)
         self.rect = self.image.get_rect (midleft = (30, 330))   #espacio que ocupa el objeto
-    def update(self) -> None:   #obligatorio asignar método update com img y rect
-        pass
+
+    def update(self):   #obligatorio asignar método update com img y rect
+        tecla = pg.key.get_pressed()    #class nave es la responsable de moverse a si misma
+        if tecla [pg.K_UP]:
+            self.rect.y -= self.velocidad
+            if self.rect.top < 0:
+                self.rect.top = 0
+        if tecla [pg.K_DOWN]:
+            self.rect.y += self.velocidad
+            if self.rect.bottom > ALTO:
+                self.rect.bottom = ALTO
+                
+            
+
+
